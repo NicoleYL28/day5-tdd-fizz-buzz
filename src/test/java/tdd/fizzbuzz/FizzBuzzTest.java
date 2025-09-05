3b89dc7da85d48c268a400ba770eaf7530f30dc3
TAMNI <tamni@oocl.com>
2025-09-05 13:44:50 +0800

    test: multiplication of three and seven test case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -56,5 +56,15 @@ public class FizzBuzzTest {
         assertThat(result).isEqualTo("Whizz");
     }
 
+    @Test
+    public void should_return_Fizz_when_countOff_given_number_is_multiplication_of_three_and_seven(){
+        int num = 21;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff(num);
+
+        assertThat(result).isEqualTo("Whizz");
+    }
+
 
 }
