1dfc143aa1b269aac3d4fe3254f075b29c2917ac
TAMNI <tamni@oocl.com>
2025-09-05 11:38:23 +0800

    test: build normal number test case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_normal_number_when_countOff_given_normal_number(){
+        int num = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff();
+
+        assertThat(result).isEqualTo("1");
+    }
+
+
 }
