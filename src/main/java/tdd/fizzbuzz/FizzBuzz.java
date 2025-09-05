0af1c11e22b71a6a18348408af50ea9e432a2849
TAMNI <tamni@oocl.com>
2025-09-05 11:47:39 +0800

    refactor: multiplication of five test case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,8 @@ public class FizzBuzz {
     public String countOff(int number) {
         if(number%3 == 0){
             return "Fizz";
+        } else if (number%5 == 0){
+            return "Bizz";
         }
         return String.valueOf(number);
     }
